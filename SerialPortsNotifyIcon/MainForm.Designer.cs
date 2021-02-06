4037f37c3a1daf1da2f5ac579193dcc3af6e5758
Lorenzo Frezza <lorenzo.frezza@uniroma1.it>
2021-02-06 10:48:26 +0100

    Added project

---
+++ SerialPortsNotifyIcon/MainForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace SerialPortsNotifyIcon
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.menuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.ContextMenuStrip = this.menuStrip;
+            this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
+            this.trayIcon.Text = "notifyIcon1";
+            this.trayIcon.Visible = true;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Name = "contextMenuStrip1";
+            this.menuStrip.Size = new System.Drawing.Size(61, 4);
+            this.menuStrip.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 58);
+            this.Name = "MainForm";
+            this.Text = "SerialPort List";
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon trayIcon;
+        private System.Windows.Forms.ContextMenuStrip menuStrip;
+    }
+}
+
---
+++ SerialPortsNotifyIcon/MainForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Management;
+using System.IO.Ports;
+
+namespace SerialPortsNotifyIcon
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+            Hide();
+            listSerialPorts();
+        }
+
+        private void listSerialPorts()
+        {
+            using var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Caption like '%(COM%'");
+            var portnames = SerialPort.GetPortNames();
+            var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
+
+            var portList = portnames.Select(n => n + " - " + ports.FirstOrDefault(s => s.Contains('(' + n + ')'))).ToList();
+
+            menuStrip.Items.Clear();
+            foreach (string s in portList)
+            {
+                menuStrip.Items.Add(s, null);
+            }
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            listSerialPorts();
+        }
+
+    }
+}
